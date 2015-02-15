3dbd13bd680d2c9f67bf287eafc94183f271ff56
Philip Chimento <philip.chimento@gmail.com>
2015-02-15 00:17:48 -0800

    Make page titles readable in sidebar
    In order to keep page titles as short as possible in the sidebar (which
    DevDocs recommends), we remove the namespace. For readability, we append
    "()" to functions, methods, and constructors, prepend ":" to properties,
    and prepend "::" to signals.

--- lib/docs/filters/gir_scraper/entries.rb
+++ lib/docs/filters/gir_scraper/entries.rb
@@ -10,8 +10,9 @@ module Docs
       # "name" is the title that the documentation page has in the left sidebar.
       def get_name
         node = at_css('h1')
-        result = node.content.strip
-        result
+        entry = node.content
+        entry.slice!(/^[A-Za-z]+\./)  # remove namespace
+        annotate_entry entry, node
       end
 
       # "type" is the heading that the documentation page is displayed under, in
@@ -31,10 +32,22 @@ module Docs
 
         # Additional entries should be marked with the entry class
         css('.entry').each do |node|
-          entries.push [node.content, node[:id]]
+          entry = annotate_entry node.content, node
+          entries.push [entry, node[:id]]
         end
 
         entries
+      end
+
+      def annotate_entry(entry, node)
+        entry.strip!
+        # node.matches('selector') would be cleaner than this, but is way slower
+        case node[:class]
+        when /\b(function|method|constructor)\b/ then entry << '()'
+        when /\bproperty\b/ then entry.prepend(':')
+        when /\bsignal\b/ then entry.prepend('::')
+        end
+        entry
       end
     end
   end
