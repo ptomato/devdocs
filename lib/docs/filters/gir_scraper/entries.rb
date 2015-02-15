a057f16858f3f34906477b7dea133384f1152d35
Philip Chimento <philip.chimento@gmail.com>
2015-02-15 00:14:26 -0800

    Organize pages in sidebar
    For now, we organize classes, enums, etc. under their own headings. Other
    things we file under special headings, in parentheses so they sshow up
    first alphabetically: functions, function typedefs, and "default" which
    in practice seems to mean constants.

--- lib/docs/filters/gir_scraper/entries.rb
+++ lib/docs/filters/gir_scraper/entries.rb
@@ -3,6 +3,10 @@ module Docs
     # EntriesFilter for g-ir-doc-tool generated documentation. This figures out
     # the metadata for each page.
     class EntriesFilter < Docs::EntriesFilter
+      FUNCTIONS_HEADING = '(Functions)'
+      FUNCTION_TYPES_HEADING = '(Function Types)'
+      CONSTANTS_HEADING = '(Constants)'
+
       # "name" is the title that the documentation page has in the left sidebar.
       def get_name
         node = at_css('h1')
@@ -13,7 +17,13 @@ module Docs
       # "type" is the heading that the documentation page is displayed under, in
       # the left sidebar.
       def get_type
-        name
+        node = at_css('h1')
+        case node[:class]
+        when 'function' then FUNCTIONS_HEADING
+        when 'callback' then FUNCTION_TYPES_HEADING
+        when 'default' then CONSTANTS_HEADING  # not sure about this
+        else name
+        end
       end
 
       def additional_entries
