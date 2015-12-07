7eca4012d75ef8c14b26d1959dc158c2fa2f7396
Philip Chimento <philip.chimento@gmail.com>
2015-12-07 01:03:01 -0800

    Highlight Javascript code
    Highlight <code> elements as well as <pre> elements with the correct MIME
    type, as well.
    
    (Ported from CoffeeScript to JS by Evan Welsh.)

--- assets/javascripts/gnome/views/pages/gir.js
+++ assets/javascripts/gnome/views/pages/gir.js
@@ -2,6 +2,7 @@
 
 app.views.GirPage = class GirPage extends app.views.BasePage {
   afterRender() {
-    this.highlightCode(this.findAll('pre[data-mime="text/x-csrc"]'), 'c');
+    this.highlightCode(this.findAll('pre[data-mime="text/x-csrc"], code[data-mime="text/x-csrc"]'), 'c')
+    this.highlightCode(this.findAll('pre[data-mime="application/javascript], code[data-mime="text/x-csrc"]'), 'javascript')
   }
 };
