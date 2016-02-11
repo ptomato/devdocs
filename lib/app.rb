e2423c3fb2bc1442f3894ead791650f93d66d37c
Philip Chimento <philip.chimento@gmail.com>
2016-02-10 23:18:06 -0800

    Restore news page and popups
    We have our own news for docs.ptomato.name:9292 now. It'll include all
    the upstream DevDocs news except for docsets which we don't display here.
    It will also include new GIR docsets that we add.

--- lib/app.rb
+++ lib/app.rb
@@ -33,7 +33,7 @@ class App < Sinatra::Application
     set :docs_path, File.join(public_folder, docs_prefix)
     set :docs_manifest_path, File.join(docs_path, 'docs.json')
     set :default_docs, %w(css dom dom_events html http javascript)
-    set :news_path, File.join(root, assets_prefix, 'javascripts', 'news.json')
+    set :news_path, File.join(root, assets_prefix, 'javascripts', 'gnome_news.json')
 
     set :csp, false
 
