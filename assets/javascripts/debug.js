213723448fe1014a1efec03110c5631ccc3cccc1
rockon999 <rockon999@users.noreply.github.com>
2018-08-02 20:15:09 -0500

    Decaffeinate debug.js.coffee.

---
+++ assets/javascripts/debug.js
@@ -0,0 +1,110 @@
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * DS203: Remove `|| {}` from converted for-own loops
+ * DS207: Consider shorter variations of null checks
+ * DS208: Avoid top-level this
+ * DS209: Avoid top-level return
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+if (!(typeof console !== 'undefined' && console !== null ? console.time : undefined) || !console.groupCollapsed) { return; }
+
+//
+// App
+//
+
+const _init = app.init;
+app.init = function() {
+  console.time('Init');
+  _init.call(app);
+  console.timeEnd('Init');
+  return console.time('Load');
+};
+
+const _start = app.start;
+app.start = function() {
+  console.timeEnd('Load');
+  console.time('Start');
+  _start.call(app, ...arguments);
+  return console.timeEnd('Start');
+};
+
+//
+// Searcher
+//
+
+const _super = app.Searcher;
+const _proto = app.Searcher.prototype;
+
+app.Searcher = function() {
+  _super.apply(this, arguments);
+
+  const _setup = this.setup.bind(this);
+  this.setup = function() {
+    console.groupCollapsed(`Search: ${this.query}`);
+    console.time('Total');
+    return _setup();
+  };
+
+  const _match = this.match.bind(this);
+  this.match = () => {
+    if (this.matcher) { console.timeEnd(this.matcher.name); }
+    return _match();
+  };
+
+  const _setupMatcher = this.setupMatcher.bind(this);
+  this.setupMatcher = function() {
+    console.time(this.matcher.name);
+    return _setupMatcher();
+  };
+
+  const _end = this.end.bind(this);
+  this.end = function() {
+    console.log(`Results: ${this.totalResults}`);
+    console.timeEnd('Total');
+    console.groupEnd();
+    return _end();
+  };
+
+  const _kill = this.kill.bind(this);
+  this.kill = function() {
+    if (this.timeout) {
+      if (this.matcher) { console.timeEnd(this.matcher.name); }
+      console.groupEnd();
+      console.timeEnd('Total');
+      console.warn('Killed');
+    }
+    return _kill();
+  };
+
+};
+
+$.extend(app.Searcher, _super);
+_proto.constructor = app.Searcher;
+app.Searcher.prototype = _proto;
+
+//
+// View tree
+//
+
+this.viewTree = function(view, level, visited) {
+  if (view == null) { view = app.document; }
+  if (level == null) { level = 0; }
+  if (visited == null) { visited = []; }
+  if (visited.indexOf(view) >= 0) { return; }
+  visited.push(view);
+
+  console.log(`%c ${Array(level + 1).join('  ')}${view.constructor.name}: ${!!view.activated}`,
+              `color:${(view.activated && 'green') || 'red'}`);
+
+  for (let key of Object.keys(view || {})) {
+    const value = view[key];
+    if ((key !== 'view') && value) {
+      if ((typeof value === 'object') && value.setupElement) {
+        this.viewTree(value, level + 1, visited);
+      } else if (value.constructor.toString().match(/Object\(\)/)) {
+        for (let k of Object.keys(value || {})) { const v = value[k]; if (v && (typeof v === 'object') && v.setupElement) { this.viewTree(v, level + 1, visited); } }
+      }
+    }
+  }
+};
