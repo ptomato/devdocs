334fe68150fcbfb6022f59e5295c2cbf3f2e51b8
rockon999 <rockon999@users.noreply.github.com>
2018-08-02 21:50:59 -0500

    Decaffeinate /application.js.coffee.

---
+++ assets/javascripts/application.js
@@ -0,0 +1,38 @@
+/*
+ * decaffeinate suggestions:
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+//= require_tree ./vendor
+
+//= require lib/license
+//= require_tree ./lib
+
+//= require app/app
+//= require app/config
+//= require_tree ./app
+
+//= require collections/collection
+//= require_tree ./collections
+
+//= require models/model
+//= require_tree ./models
+
+//= require views/view
+//= require_tree ./views
+
+//= require_tree ./templates
+
+//= require tracking
+
+var init = function() {
+  document.removeEventListener('DOMContentLoaded', init, false);
+
+  if (document.body) {
+    return app.init();
+  } else {
+    return setTimeout(init, 42);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', init, false);
