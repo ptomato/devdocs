6fe5cffa32bbba2e99d1fa2b5c1b5349cedf49c0
rockon999 <rockon999@users.noreply.github.com>
2018-08-02 21:57:17 -0500

    Cleanup and fix scope of /templates/.

--- assets/javascripts/templates/base.js
+++ assets/javascripts/templates/base.js
@@ -1,18 +1,14 @@
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-app.templates.render = function(name, value, ...args) {
+app.templates.render = function (name, value, ...args) {
   const template = app.templates[name];
 
   if (Array.isArray(value)) {
     let result = '';
-    for (let val of Array.from(value)) { result += template(val, ...Array.from(args)); }
+    for (let val of value) {
+      result += template(val, ...args);
+    }
     return result;
   } else if (typeof template === 'function') {
-    return template(value, ...Array.from(args));
+    return template(value, ...args);
   } else {
     return template;
   }
--- assets/javascripts/templates/path_tmpl.js
+++ assets/javascripts/templates/path_tmpl.js
@@ -1,13 +1,14 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-const arrow = "<svg class=\"_path-arrow\"><use xlink:href=\"#icon-dir\"/></svg>";
+{
+  const arrow = "<svg class=\"_path-arrow\"><use xlink:href=\"#icon-dir\"/></svg>";
 
-app.templates.path = function(doc, type, entry) {
-  let html = `<a href="${doc.fullPath()}" class="_path-item _icon-${doc.icon}">${doc.fullName}</a>`;
-  if (type) { html += `${arrow}<a href="${type.fullPath()}" class="_path-item">${type.name}</a>`; }
-  if (entry) { html += `${arrow}<span class="_path-item">${$.escape(entry.name)}</span>`; }
-  return html;
-};
+  app.templates.path = function (doc, type, entry) {
+    let html = `<a href="${doc.fullPath()}" class="_path-item _icon-${doc.icon}">${doc.fullName}</a>`;
+    if (type) {
+      html += `${arrow}<a href="${type.fullPath()}" class="_path-item">${type.name}</a>`;
+    }
+    if (entry) {
+      html += `${arrow}<span class="_path-item">${$.escape(entry.name)}</span>`;
+    }
+    return html;
+  };
+}
