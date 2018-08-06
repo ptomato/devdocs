67ed95274f43e4da158e44e18af930c72cf567d2
rockon999 <rockon999@users.noreply.github.com>
2018-08-05 21:18:43 -0500

    Cleanup static variables in app.Settings.

--- assets/javascripts/app/settings.js
+++ assets/javascripts/app/settings.js
@@ -1,155 +1,150 @@
-(function () {
-  let PREFERENCE_KEYS = undefined;
-  let INTERNAL_KEYS = undefined;
-  const Cls = (app.Settings = class Settings {
-    static initClass() {
-      PREFERENCE_KEYS = [
-        'hideDisabled',
-        'hideIntro',
-        'manualUpdate',
-        'fastScroll',
-        'arrowScroll',
-        'docs',
-        'dark',
-        'layout',
-        'size',
-        'tips'
-      ];
+app.Settings = class Settings {
+  static initClass() {
+    this.PREFERENCE_KEYS = [
+      'hideDisabled',
+      'hideIntro',
+      'manualUpdate',
+      'fastScroll',
+      'arrowScroll',
+      'docs',
+      'dark',
+      'layout',
+      'size',
+      'tips'
+    ];
 
-      INTERNAL_KEYS = [
-        'count',
-        'schema',
-        'version',
-        'news'
-      ];
+    this.INTERNAL_KEYS = [
+      'count',
+      'schema',
+      'version',
+      'news'
+    ];
 
-      this.defaults = {
-        count: 0,
-        hideDisabled: false,
-        hideIntro: false,
-        news: 0,
-        manualUpdate: false,
-        schema: 1
-      };
+    this.defaults = {
+      count: 0,
+      hideDisabled: false,
+      hideIntro: false,
+      news: 0,
+      manualUpdate: false,
+      schema: 1
+    };
+  }
+
+  constructor() {
+    this.store = new CookieStore;
+    this.cache = {};
+  }
+
+  get(key) {
+    let left;
+    if (this.cache.hasOwnProperty(key)) {
+      return this.cache[key];
+    }
+    return this.cache[key] = (left = this.store.get(key)) != null ? left : this.constructor.defaults[key];
+  }
+
+  set(key, value) {
+    this.store.set(key, value);
+    delete this.cache[key];
+  }
+
+  del(key) {
+    this.store.del(key);
+    delete this.cache[key];
+  }
+
+  hasDocs() {
+    try {
+      return !!this.store.get('docs');
+    } catch (error) {}
+  }
+
+  getDocs() {
+    let docs = this.store.get('docs');
+
+    if (docs) {
+      return docs.split('/');
     }
 
-    constructor() {
-      this.store = new CookieStore;
-      this.cache = {};
-    }
+    return app.config.default_docs;
+  }
 
-    get(key) {
-      let left;
-      if (this.cache.hasOwnProperty(key)) {
-        return this.cache[key];
+  setDocs(docs) {
+    this.set('docs', docs.join('/'));
+  }
+
+  getTips() {
+    let tips = this.store.get('tips');
+    if (tips != null) {
+      return tips.split('/');
+    }
+    return [];
+  }
+
+  setTips(tips) {
+    this.set('tips', tips.join('/'));
+  }
+
+  setLayout(name, enable) {
+    const layout = (this.store.get('layout') || '').split(' ');
+    $.arrayDelete(layout, '');
+
+    if (enable) {
+      if (layout.indexOf(name) === -1) {
+        layout.push(name);
       }
-      return this.cache[key] = (left = this.store.get(key)) != null ? left : this.constructor.defaults[key];
+    } else {
+      $.arrayDelete(layout, name);
     }
 
-    set(key, value) {
-      this.store.set(key, value);
-      delete this.cache[key];
+    if (layout.length > 0) {
+      this.set('layout', layout.join(' '));
+    } else {
+      this.del('layout');
     }
+  }
 
-    del(key) {
-      this.store.del(key);
-      delete this.cache[key];
+  hasLayout(name) {
+    const layout = (this.store.get('layout') || '').split(' ');
+    return layout.indexOf(name) !== -1;
+  }
+
+  setSize(value) {
+    this.set('size', value);
+  }
+
+  dump() {
+    return this.store.dump();
+  }
+
+  export () {
+    const data = this.dump();
+    for (let key of Settings.INTERNAL_KEYS) {
+      delete data[key];
     }
+    return data;
+  }
 
-    hasDocs() {
-      try {
-        return !!this.store.get('docs');
-      } catch (error) {}
-    }
-
-    getDocs() {
-      let docs = this.store.get('docs');
-
-      if (docs) {
-        return docs.split('/');
-      }
-      
-      return app.config.default_docs;
-    }
-
-    setDocs(docs) {
-      this.set('docs', docs.join('/'));
-    }
-
-    getTips() {
-      let tips = this.store.get('tips');
-      if (tips != null) {
-        return tips.split('/');
-      }
-      return [];
-    }
-
-    setTips(tips) {
-      this.set('tips', tips.join('/'));
-    }
-
-    setLayout(name, enable) {
-      const layout = (this.store.get('layout') || '').split(' ');
-      $.arrayDelete(layout, '');
-
-      if (enable) {
-        if (layout.indexOf(name) === -1) {
-          layout.push(name);
-        }
-      } else {
-        $.arrayDelete(layout, name);
-      }
-
-      if (layout.length > 0) {
-        this.set('layout', layout.join(' '));
-      } else {
-        this.del('layout');
-      }
-    }
-
-    hasLayout(name) {
-      const layout = (this.store.get('layout') || '').split(' ');
-      return layout.indexOf(name) !== -1;
-    }
-
-    setSize(value) {
-      this.set('size', value);
-    }
-
-    dump() {
-      return this.store.dump();
-    }
-
-    export () {
-      const data = this.dump();
-      for (let key of INTERNAL_KEYS) {
-        delete data[key];
-      }
-      return data;
-    }
-
-    import (data) {
-      let value;
-      const object = this.export();
-      for (var key in object) {
-        value = object[key];
-        if (!data.hasOwnProperty(key)) {
-          this.del(key);
-        }
-      }
-      for (key in data) {
-        value = data[key];
-        if (PREFERENCE_KEYS.indexOf(key) !== -1) {
-          this.set(key, value);
-        }
+  import (data) {
+    let value;
+    const object = this.export();
+    for (var key in object) {
+      value = object[key];
+      if (!data.hasOwnProperty(key)) {
+        this.del(key);
       }
     }
-
-    reset() {
-      this.store.reset();
-      this.cache = {};
+    for (key in data) {
+      value = data[key];
+      if (this.PREFERENCE_KEYS.indexOf(key) !== -1) {
+        this.set(key, value);
+      }
     }
-  });
-  Cls.initClass();
-  return Cls;
-})();
+  }
+
+  reset() {
+    this.store.reset();
+    this.cache = {};
+  }
+}
+app.Settings.initClass();
