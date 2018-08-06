218a713966ccaa6884596e2725bb8e17312731a2
rockon999 <rockon999@users.noreply.github.com>
2018-08-06 01:52:14 -0500

    Add GJS Branding. - Fix lib/app override.

--- assets/javascripts/gnome/views/layout/document.js
+++ assets/javascripts/gnome/views/layout/document.js
@@ -2,6 +2,6 @@
   const SUPER_setTitle = app.views.Document.prototype.setTitle;
 
   app.views.Document.prototype.setTitle = function(title) {
-    SUPER_setTitle.call(this, title ? `${title} — DevDocs` : 'GNOME Javascript Docs');
+    SUPER_setTitle.call(this, title ? `${title} — GJS Docs` : 'GJS Docs');
   }
 }
