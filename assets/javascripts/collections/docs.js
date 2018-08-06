f9282a33afb648207c5a60c33222936a2af20b64
rockon999 <rockon999@users.noreply.github.com>
2018-08-05 21:21:20 -0500

    Cleanup static variables in app.collections.Docs.

--- assets/javascripts/collections/docs.js
+++ assets/javascripts/collections/docs.js
@@ -1,125 +1,119 @@
-(function () {
-  let NORMALIZE_VERSION_RGX = undefined;
-  let NORMALIZE_VERSION_SUB = undefined;
-  let CONCURRENCY = undefined;
-  const Cls = (app.collections.Docs = class Docs extends app.Collection {
-    static initClass() {
-      this.model = 'Doc';
+app.collections.Docs = class Docs extends app.Collection {
+  static initClass() {
+    this.model = 'Doc';
 
-      NORMALIZE_VERSION_RGX = /\.(\d)$/;
-      NORMALIZE_VERSION_SUB = '.0$1';
+    this.NORMALIZE_VERSION_RGX = /\.(\d)$/;
+    this.NORMALIZE_VERSION_SUB = '.0$1';
 
-      // Load models concurrently.
-      // It's not pretty but I didn't want to import a promise library only for this.
-      CONCURRENCY = 3;
-    }
+    // Load models concurrently.
+    // It's not pretty but I didn't want to import a promise library only for this.
+    this.CONCURRENCY = 3;
+  }
 
-    findBySlug(slug) {
-      return this.findBy('slug', slug) || this.findBy('slug_without_version', slug);
-    }
-    sort() {
-      return this.models.sort(function (a, b) {
-        if (a.name === b.name) {
-          if (!a.version || (a.version.replace(NORMALIZE_VERSION_RGX, NORMALIZE_VERSION_SUB) > b.version.replace(NORMALIZE_VERSION_RGX, NORMALIZE_VERSION_SUB))) {
-            return -1;
-          } else {
-            return 1;
-          }
-        } else if (a.name.toLowerCase() > b.name.toLowerCase()) {
-          return 1;
-        } else {
+  findBySlug(slug) {
+    return this.findBy('slug', slug) || this.findBy('slug_without_version', slug);
+  }
+  sort() {
+    return this.models.sort(function (a, b) {
+      if (a.name === b.name) {
+        if (!a.version || (a.version.replace(Docs.NORMALIZE_VERSION_RGX, Docs.NORMALIZE_VERSION_SUB) > b.version.replace(Docs.NORMALIZE_VERSION_RGX, Docs.NORMALIZE_VERSION_SUB))) {
           return -1;
-        }
-      });
-    }
-    load(onComplete, onError, options) {
-      let i = 0;
-
-      var next = () => {
-        if (i < this.models.length) {
-          this.models[i].load(next, fail, options);
-        } else if (i === ((this.models.length + CONCURRENCY) - 1)) {
-          onComplete();
-        }
-        i++;
-      };
-
-      var fail = function (...args) {
-        if (onError) {
-          onError(...(args || []));
-          onError = null;
-        }
-        next();
-      };
-
-      for (let j = 0, end = CONCURRENCY, asc = 0 <= end; asc ? j < end : j > end; asc ? j++ : j--) {
-        next();
-      }
-    }
-
-    clearCache() {
-      for (let doc of this.models) {
-        doc.clearCache();
-      }
-    }
-
-    uninstall(callback) {
-      let i = 0;
-      var next = () => {
-        if (i < this.models.length) {
-          this.models[i++].uninstall(next, next);
         } else {
-          callback();
+          return 1;
         }
-      };
+      } else if (a.name.toLowerCase() > b.name.toLowerCase()) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+  }
+  load(onComplete, onError, options) {
+    let i = 0;
+
+    var next = () => {
+      if (i < this.models.length) {
+        this.models[i].load(next, fail, options);
+      } else if (i === ((this.models.length + Docs.CONCURRENCY) - 1)) {
+        onComplete();
+      }
+      i++;
+    };
+
+    var fail = function (...args) {
+      if (onError) {
+        onError(...(args || []));
+        onError = null;
+      }
+      next();
+    };
+
+    for (let j = 0, end = Docs.CONCURRENCY, asc = 0 <= end; asc ? j < end : j > end; asc ? j++ : j--) {
       next();
     }
+  }
 
-    getInstallStatuses(callback) {
-      app.db.versions(this.models, function (statuses) {
-        if (statuses) {
-          for (let key in statuses) {
-            const value = statuses[key];
-            statuses[key] = {
-              installed: !!value,
-              mtime: value
-            };
-          }
-        }
-        callback(statuses);
-      });
+  clearCache() {
+    for (let doc of this.models) {
+      doc.clearCache();
     }
+  }
 
-    checkForUpdates(callback) {
-      this.getInstallStatuses(statuses => {
-        let i = 0;
-        if (statuses) {
-          for (let slug in statuses) {
-            const status = statuses[slug];
-            if (this.findBy('slug', slug).isOutdated(status)) {
-              i += 1;
-            }
-          }
-        }
-        callback(i);
-      });
-    }
+  uninstall(callback) {
+    let i = 0;
+    var next = () => {
+      if (i < this.models.length) {
+        this.models[i++].uninstall(next, next);
+      } else {
+        callback();
+      }
+    };
+    next();
+  }
 
-    updateInBackground() {
-      this.getInstallStatuses(statuses => {
-        if (!statuses) {
-          return;
+  getInstallStatuses(callback) {
+    app.db.versions(this.models, function (statuses) {
+      if (statuses) {
+        for (let key in statuses) {
+          const value = statuses[key];
+          statuses[key] = {
+            installed: !!value,
+            mtime: value
+          };
         }
+      }
+      callback(statuses);
+    });
+  }
+
+  checkForUpdates(callback) {
+    this.getInstallStatuses(statuses => {
+      let i = 0;
+      if (statuses) {
         for (let slug in statuses) {
           const status = statuses[slug];
-          const doc = this.findBy('slug', slug);
-          if (doc.isOutdated(status)) {
-            doc.install($.noop, $.noop);
+          if (this.findBy('slug', slug).isOutdated(status)) {
+            i += 1;
           }
         }
-      });
-    }
-  });
-  Cls.initClass();
-  return Cls;
-})();
+      }
+      callback(i);
+    });
+  }
+
+  updateInBackground() {
+    this.getInstallStatuses(statuses => {
+      if (!statuses) {
+        return;
+      }
+      for (let slug in statuses) {
+        const status = statuses[slug];
+        const doc = this.findBy('slug', slug);
+        if (doc.isOutdated(status)) {
+          doc.install($.noop, $.noop);
+        }
+      }
+    });
+  }
+}
+app.collections.Docs.initClass();
