0e9cdfe9c10b0d0b969521a141f2a3df008e582c
rockon999 <rockon999@users.noreply.github.com>
2018-08-05 22:37:21 -0500

    Add GIR frontend page.

---
+++ assets/javascripts/gnome/views/pages/gir.js
@@ -0,0 +1,8 @@
+//= require views/pages/base
+
+app.views.GirPage = class GirPage extends app.views.BasePage {
+  afterRender() {
+    this.highlightCode(this.findAll('pre[data-mime="text/x-csrc"], code[data-mime="text/x-csrc"]'), 'c')
+    this.highlightCode(this.findAll('pre[data-mime="application/javascript], code[data-mime="text/x-csrc"]'), 'javascript')
+  }
+};
