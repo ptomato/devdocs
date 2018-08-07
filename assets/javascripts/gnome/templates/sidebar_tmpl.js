63ac402e5b5fb35db1148c9ef39c856f01249890
rockon999 <rockon999@users.noreply.github.com>
2018-08-06 20:53:35 -0500

    Limit type->entry redirecting to GIR docs.

--- assets/javascripts/gnome/templates/sidebar_tmpl.js
+++ assets/javascripts/gnome/templates/sidebar_tmpl.js
@@ -1,6 +1,6 @@
 $.extendTemplate('sidebarEntry', function (parent, entry) {
   // Only alter GIR docs
-  if (entry.doc.type === 'gir') {
+  if (entry.doc && entry.doc.type === 'gir') {
     // If the type of the entry has an identical name, it has been redirected so we can hide it.
     if (entry.type === entry.name) {
       return '';
