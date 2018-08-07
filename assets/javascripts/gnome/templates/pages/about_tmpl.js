2bcdb5502eac16815b2a202f20c3bbfbe6d0629c
rockon999 <rockon999@users.noreply.github.com>
2018-08-06 20:43:06 -0500

    Update about page with downstream information.

--- assets/javascripts/gnome/templates/pages/about_tmpl.js
+++ assets/javascripts/gnome/templates/pages/about_tmpl.js
@@ -4,72 +4,54 @@
     <h3 class="_toc-title">Table of Contents</h3>
     <ul class="_toc-list">
       <li><a href="#copyright">Copyright</a>
-      <li><a href="#plugins">Plugins</a>
       <li><a href="#faq">FAQ</a>
       <li><a href="#credits">Credits</a>
       <li><a href="#privacy">Privacy Policy</a>
     </ul>
   </nav>
 
-  <h1 class="_lined-heading">DevDocs: API Documentation Browser</h1>
-  <p>DevDocs combines multiple API documentations in a clean and organized web UI with instant search, offline support, mobile version, dark theme, keyboard shortcuts, and more.
+  <h1 class="_lined-heading">GJS Docs: Powered By DevDocs</h1>
+  <p>GJS Docs utilizes DevDocs' ability to combine multiple API documentations to explore the GNOME API documentation for GJS. DevDocs provides a clean, organized web UI with instant search, offline support, mobile version, dark theme, keyboard shortcuts, and much more!
   <ul>
-    <li>Created and maintained by <a href="https://thibaut.me">Thibaut Courouble</a>
+    <li>GJS Docs is maintained by Philip Chimento</li>
+    <li>DevDocs was created by <a href="https://thibaut.me">Thibaut Courouble</a>
     <li>Free and <a href="https://github.com/freeCodeCamp/devdocs">open source</a>
-        <iframe class="_github-btn" src="https://ghbtns.com/github-btn.html?user=freeCodeCamp&repo=devdocs&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="100" height="20" tabindex="-1"></iframe>
+        <iframe class="_github-btn" src="https://ghbtns.com/github-btn.html?user=ptomato&repo=devdocs&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="100" height="20" tabindex="-1"></iframe>
   </ul>
   <p>To keep up-to-date with the latest news:
   <ul>
-    <li>Follow <a href="https://twitter.com/DevDocs">@DevDocs</a> on Twitter
-    <li>Watch the repository on <a href="https://github.com/freeCodeCamp/devdocs/subscription">GitHub</a>
-    <li>Join the <a href="https://groups.google.com/d/forum/devdocs">mailing list</a>
+    <li>Watch the repository on <a href="https://github.com/ptomato/devdocs/subscription">GitHub</a>
   </ul>
-
-  <h2 class="_lined-heading">About the GNOME JavaScript documentation</h2>
-  <p>Generated using a modified <a href="https://github.com/ptomato/gobject-introspection/tree/wip/ptomato/devdocs">g-ir-doc-tool</a>.
-     An experiment demoed at the Developer Experience hackfest 2016.
-
 
   <h2 class="_block-heading" id="copyright">Copyright and License</h2>
   <p class="_note">
-    <strong>Copyright 2013&ndash;2018 Thibaut Courouble and <a href="https://github.com/freeCodeCamp/devdocs/graphs/contributors">other contributors</a></strong><br>
+    <strong>Copyright 2013&ndash;2018 Thibaut Courouble, Philip Chimento, Evan Welsh and <a href="https://github.com/ptomato/devdocs/graphs/contributors">other contributors</a></strong><br>
     This software is licensed under the terms of the Mozilla Public License v2.0.<br>
-    You may obtain a copy of the source code at <a href="https://github.com/ptomato/devdocs">github.com/ptomato/devdocs</a> or of the original DevDocs' source code at <a href="https://github.com/freeCodeCamp/devdocs">github.com/freeCodeCamp/devdocs</a>.<br>
-    For more information, see the <a href="https://github.com/freeCodeCamp/devdocs/blob/master/COPYRIGHT">COPYRIGHT</a>
-    and <a href="https://github.com/freeCodeCamp/devdocs/blob/master/LICENSE">LICENSE</a> files.
-
-  <!--h2 class="_block-heading" id="plugins">Plugins and Extensions</h2>
-  <ul>
-    <li><a href="https://chrome.google.com/webstore/detail/devdocs/mnfehgbmkapmjnhcnbodoamcioleeooe">Chrome web app</a>
-    <li><a href="https://github.com/egoist/devdocs-app">Desktop app</a>
-    <li><a href="https://sublime.wbond.net/packages/DevDocs">Sublime Text package</a>
-    <li><a href="https://atom.io/packages/devdocs">Atom package</a>
-    <li><a href="https://marketplace.visualstudio.com/items?itemName=deibit.devdocs">Visual Studio Code extension</a>
-    <li><a href="https://github.com/yannickglt/alfred-devdocs">Alfred workflow</a>
-    <li><a href="https://github.com/search?q=topic%3Adevdocs&type=Repositories">More…</a>
-  </ul-->
+    You may obtain a copy of the source code at <a href="https://github.com/ptomato/devdocs">github.com/ptomato/devdocs</a>.<br>
+    For more information, see the <a href="https://github.com/ptomato/devdocs/blob/master/COPYRIGHT">COPYRIGHT</a>
+    and <a href="https://github.com/ptomato/devdocs/blob/master/LICENSE">LICENSE</a> files.
 
   <h2 class="_block-heading" id="faq">Questions & Answers</h2>
   <dl>
     <dt>Where can I suggest new docs and features?
-    <dd>Find <strong>ptomato</strong> in #javascript on irc.gnome.org, or post to <a href="https://mail.gnome.org/mailman/listinfo/javascript-list">javascript-list</a>.
+    <dd>If you have a specific feature request related to DevDocs, add it to the <a href="https://github.com/freeCodeCamp/devdocs/issues">issue tracker</a>.<br>
+        If your feature is related to GNOME documentation please request it <a href="https://github.com/ptomato/devdocs/issues">here</a>.
     <dt>Where can I report bugs?
-    <dd>Report bugs with the documentation in the <a href="https://github.com/ptomato/devdocs/issues">issue tracker</a>.
-        Bugs in the DevDocs app go to <a href="https://github.com/freeCodeCamp/devdocs/issues">DevDocs' own issue tracker</a>.
+    <dd>In the <a href="https://github.com/ptomato/devdocs/issues">issue tracker</a>. Thanks!
   </dl>
 
   <h2 class="_block-heading" id="credits">Credits</h2>
 
   <p><strong>Special thanks to:</strong>
   <ul>
-    <li>Everaldo Canuto, Patrick Griffis, and Dustin Falgout for helping get this site back online
-    <li><a href="https://nuveo.com.br">Nuveo</a> for sponsoring the hosting
-    <!-- The next three don't apply to us, we're not getting sponsored by them.
-      The C/C++ logo is not used now, but will be later. -->
-    <!--li><a href="https://out.devdocs.io/s/maxcdn">MaxCDN</a>, <a href="https://sentry.io/">Sentry</a> and <a href="https://get.gaug.es/?utm_source=devdocs&utm_medium=referral&utm_campaign=sponsorships" title="Real Time Web Analytics">Gauges</a> for offering a free account to DevDocs
+    <!--<li><a href="https://out.devdocs.io/s/maxcdn">MaxCDN</a>, <a href="https://sentry.io/">Sentry</a> and <a href="https://get.gaug.es/?utm_source=devdocs&utm_medium=referral&utm_campaign=sponsorships" title="Real Time Web Analytics">Gauges</a> for offering a free account to DevDocs
     <li><a href="https://out.devdocs.io/s/maxcdn">MaxCDN</a>, <a href="https://out.devdocs.io/s/shopify">Shopify</a>, <a href="https://out.devdocs.io/s/jetbrains">JetBrains</a> and <a href="https://out.devdocs.io/s/code-school">Code School</a> for sponsoring DevDocs in the past
     <li><a href="https://www.heroku.com">Heroku</a> and <a href="https://newrelic.com/">New Relic</a> for providing awesome free service
     <li><a href="https://www.jeremykratz.com/">Jeremy Kratz</a> for the C/C++ logo-->
+    <li>Everaldo Canuto, Patrick Griffis, and Dustin Falgout for helping get this site back online
+    <li><a href="https://nuveo.com.br">Nuveo</a> for sponsoring the hosting
+    <li>Daniel Bruce for the <a href="http://www.entypo.com">Entypo</a> pictograms.
+    <li><a href="https://icons8.com/icon/set/programming">Icons8</a> for the programming category icons.
   </ul>
 
   <div class="_table">
@@ -84,18 +66,17 @@
 
   <h2 class="_block-heading" id="privacy">Privacy Policy</h2>
   <ul>
-    <li><a href="https://devdocs.io">devdocs.io</a> ("App") is operated by Thibaut Courouble ("We").
+    <li><a href="https://docs.gjs.guide">GJS Docs</a> ("App") is operated by the GNOME Community ("We").
     <li>We do not collect personal information.
-    <li>We use Google Analytics, Gauges and Sentry to collect anonymous traffic information and improve the app.
+    <li>While upstream utilizes Google Analytics, Gauges and Sentry to collect anonymous traffic information and improve the app, we have <b>disabled</b> these components.
     <li>The app uses cookies to store user preferences.
     <li>By using the app, you signify your acceptance of this policy. If you do not agree to this policy, please do not use the app.
-    <li>If you have any questions regarding privacy, please email <a href="mailto:thibaut@devdocs.io">thibaut@devdocs.io</a>.
+    <li>If you have any questions regarding privacy, please file a GitHub issue in the <a href="https://github.com/ptomato/devdocs">GJS Docs repo</a>.
   </ul>\
-`;
+  `;
 
   var credits = [
-    [
-      'Cally',
+    ['Cally',
       '2008, 2009, 2010 Igalia S.L.',
       'GNU FDL 1.1',
       'https://developer.gnome.org/cally/stable/'
