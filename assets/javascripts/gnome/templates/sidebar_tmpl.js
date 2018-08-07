c4dce3c2cf75b2d115ba4647ed004a6980562089
rockon999 <rockon999@users.noreply.github.com>
2018-08-06 20:42:40 -0500

    Add sidebar icons.

--- assets/javascripts/gnome/templates/sidebar_tmpl.js
+++ assets/javascripts/gnome/templates/sidebar_tmpl.js
@@ -1,8 +1,22 @@
 $.extendTemplate('sidebarEntry', function (parent, entry) {
-  // If the type of the entry has an identical name, it has been redirected so we can hide it.
+  // Only alter GIR docs
+  if (entry.doc.type === 'gir') {
+    // If the type of the entry has an identical name, it has been redirected so we can hide it.
+    if (entry.type === entry.name) {
+      return '';
+    }
 
-  if (entry.type === entry.name) {
-    return '';
+    let iconClass = "_prog-icon";
+
+    if (entry.name.endsWith("()") || entry.name.startsWith("vfunc")) {
+      iconClass += " _prog-function-icon"
+    } else if (entry.name.startsWith("::")) {
+      iconClass += " _prog-signal-icon"
+    } else {
+      iconClass += " _prog-property-icon"
+    }
+
+    return `<a href="${entry.fullPath()}" class="_list-item _list-item-no-children _list-hover ${iconClass}" tabindex="-1">${$.escape(entry.name)}</a>`;
   }
 
   return parent.call(this, entry);
